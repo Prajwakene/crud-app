b105c552e144f5186cc3bf034ef6c11155ae9aa9
Prajwal Kene <prajwalnk123@gmail.com>
2023-08-31 23:58:38 +0530

    setting up routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,11 +8,53 @@ const {
   deleteUser,
 } = require('./controllers/userController');
 
-// Define routes for CRUD operations
-router.post('/users', createUser);
-router.get('/users', getUserList);
-router.get('/users/:id', getUserById);
-router.put('/users/:id', updateUser);
-router.delete('/users/:id', deleteUser);
+// Create a new user
+router.post('/users', async (req, res) => {
+  try {
+    await createUser(req, res);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Server Error' });
+  }
+});
 
+// Retrieve a list of user records
+router.get('/users', async (req, res) => {
+  try {
+    await getUserList(req, res);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+// Retrieve a single user record by ID
+router.get('/users/:id', async (req, res) => {
+  try {
+    await getUserById(req, res);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+// Update a user record by ID
+router.put('/users/:id', async (req, res) => {
+  try {
+    await updateUser(req, res);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+// Delete a user record by ID
+router.delete('/users/:id', async (req, res) => {
+  try {
+    await deleteUser(req, res);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Server Error' });
+  }
+});
 module.exports = router;
