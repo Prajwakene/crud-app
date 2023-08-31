4569e29ae7915ac4d2d0c6e23360806154d8c285
Prajwal Kene <prajwalnk123@gmail.com>
2023-09-01 00:27:39 +0530

    making changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import './App.css';
+import './App.css'
 import UserList from './components/Users/UserList';
 import CreateUser from './components/Users/CreateUser';
 import EditUser from './components/Users/EditUser';
